d2d4e4638ce2df6809d0803ee8f96776e5a48e30
test <test@test.com>
2019-11-08 15:45:01 +0000

    another change

--- .travis/deploy_docker_image.sh
+++ .travis/deploy_docker_image.sh
@@ -3,5 +3,5 @@
 docker login -u $DOCKER_USERNAME -p $DOCKER_PWD
 TAG="latest"
 
-docker build -t scrape-bbc:$TAG .
-docker push scrape-bbc:$TAG
+docker build -t $DOCKER_USERNAME/scrape-bbc:$TAG .
+docker push $DOCKER_USERNAME/scrape-bbc:$TAG
